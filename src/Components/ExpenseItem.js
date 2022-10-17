eede3e63c8f10d9d91fbdc3023ccdbd1ae6bd7c8
thajunnisaph <thajunnisaph123@gmail.com>
2022-10-17 23:30:27 +0530

    Expense items with 3 data

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+  return( <div>
+             <h4>Expense Items</h4>
+             <p>Food Rs 10</p>
+             <p> Petrol Rs 100</p>
+             <p>Movies Rs 200 </p>
+            </div>
+         );
+}
+export default ExpenseItem;
