25c8473f72ff6cd12704ee023783825ae9ba88de
thajunnisaph <thajunnisaph123@gmail.com>
2022-10-20 15:30:50 +0530

    ExpenseDetails ,Date new components added

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,14 @@
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
-  
+ 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div>{props.location}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
+      <ExpenseDetails
+       amount={props.amount}
+       title={props.title} />
     </div>
   );
 }
