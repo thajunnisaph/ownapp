69fe605e5b1fb4fd0b4cec0e4c53f375346014f4
thajunnisaph <thajunnisaph123@gmail.com>
2022-10-21 18:42:48 +0530

    add a delete expense button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,21 +4,14 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
-  // return React.createElement(
-  //   "div",
-  //   { className: "expense-item" },
-  //   React.createElement(ExpenseDate, { date: props.date }),
-  //   React.createElement(
-  //     "div",
-  //     {},
-  //     React.createElement("h1", {}, "Expense 1"),
-  //     React.createElement("h1", {}, "Expense 1")
-  //   )
-  // );
+const clickHandler = () => {
+console.log('clicked');
+}
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails amount={props.amount} title={props.title} />
+      <button onClick={clickHandler}>Delete Expense</button>
     </Card>
   );
 };
