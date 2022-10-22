3f81b75df3e34d9a5831d2e23d1920dd0d260f9e
thajunnisaph <thajunnisaph123@gmail.com>
2022-10-22 23:13:17 +0530

    add a button change expense ,when click on this change this expense to 100$.

--- src/Components/Expenses/ExpenseDetails.js
+++ src/Components/Expenses/ExpenseDetails.js
@@ -1,12 +1,22 @@
-
+import React, { useState } from "react";
 import "./ExpenseItem.css";
-const ExpenseDetails =(props) =>{
-    return (
-		<div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-);
-
-}
+const ExpenseDetails = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+  const clickHandlerprice = () => {
+    setAmount("100$");
+  };
+  const clickHandler = () => {
+    setTitle("Updated");
+    console.log(title);
+  };
+  return (
+    <div className="expense-item__description">
+      <h2>{title}</h2>
+      <div className="expense-item__price">${amount}</div>
+      <button onClick={clickHandler}>Edit Title</button>
+      <button onClick={clickHandlerprice}>Change Expense</button>
+    </div>
+  );
+};
 export default ExpenseDetails;
--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,14 +4,12 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
-const clickHandler = () => {
-console.log('clicked');
-}
+  
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails amount={props.amount} title={props.title} />
-      <button onClick={clickHandler}>Delete Expense</button>
+      
     </Card>
   );
 };
