44959d62332ce1cd5e6ca3ff5705c0e5ced043bb
thajunnisaph <thajunnisaph123@gmail.com>
2022-10-28 16:55:13 +0530

    add a cancel button in form and make it functional,also add some coditional rendering

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -55,6 +55,7 @@ const ExpenseForm = (props) => {
     setEnteredAmount('');
     setEnteredDate('');
   };
+
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
@@ -84,6 +85,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = (props) => {
+  const [formStatus, setFormStatus] = useState(false);
+  const formHandler = () => {
+    setFormStatus(true);
+  };
+  const cancelclickHandler = () =>{
+    setFormStatus(false);
+  }
   const saveExpenseDataHandler = (enteredexpensedata) => {
     const expensedata = {
       ...enteredexpensedata,
       id: Math.random().toString(),
     };
     props.onAddExpense(expensedata);
+    setFormStatus(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onsubmitexpensedata={saveExpenseDataHandler} />
-      
+      {!formStatus && <button onClick={formHandler}>Add New Expense</button>}
+      {formStatus && (
+        <ExpenseForm onsubmitexpensedata={saveExpenseDataHandler} 
+        onCancel={cancelclickHandler}/>
+      )}
     </div>
   );
 };
